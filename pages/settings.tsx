23c1ef41b59e32a016b46969569ff749f0805d80
benjaponum <benjaponum@gmail.com>
2022-01-09 23:53:08 +0900

    made settings page authentication-required.

--- pages/settings.tsx
+++ pages/settings.tsx
@@ -7,26 +7,29 @@ import {
   janCodesDictionaryFileUrl,
   updateJanDictionaryUrl, updateWriteInConfigurationsUrl, writeInConfigurationsFileUrl
 } from "../settings"
+import {AuthenticationRequired} from "../components/functional/AuthenticationRequired"
 
 const Settings: NextPage = () => {
   return <Layout>
-    <Box sx={{margin: 2, marginTop: 5}}>
-      <Typography variant={"h5"} sx={{borderBottom: "1px solid black"}}>設定</Typography>
-      <UpdateSettings
-        key={"JANコード設定"}
-        settingName={"JANコード設定"}
-        fileUrl={janCodesDictionaryFileUrl}
-        getDataUrl={getJanDictionaryFromGoogleSpreadsheetsUrl}　
-        updateDataUrl={updateJanDictionaryUrl}
-      />
-      <UpdateSettings
-        key={"書き込み設定"}
-        settingName={"書き込み設定"}
-        fileUrl={writeInConfigurationsFileUrl}
-        getDataUrl={getWriteInConfigurationsFromGoogleSpreadsheetsUrl}
-        updateDataUrl={updateWriteInConfigurationsUrl}
-      />
-    </Box>
+    <AuthenticationRequired>
+      <Box sx={{margin: 2, marginTop: 5}}>
+        <Typography variant={"h5"} sx={{borderBottom: "1px solid black"}}>設定</Typography>
+        <UpdateSettings
+          key={"JANコード設定"}
+          settingName={"JANコード設定"}
+          fileUrl={janCodesDictionaryFileUrl}
+          getDataUrl={getJanDictionaryFromGoogleSpreadsheetsUrl}
+          updateDataUrl={updateJanDictionaryUrl}
+        />
+        <UpdateSettings
+          key={"書き込み設定"}
+          settingName={"書き込み設定"}
+          fileUrl={writeInConfigurationsFileUrl}
+          getDataUrl={getWriteInConfigurationsFromGoogleSpreadsheetsUrl}
+          updateDataUrl={updateWriteInConfigurationsUrl}
+        />
+      </Box>
+    </AuthenticationRequired>
   </Layout>
 }
 
