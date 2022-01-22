c490fb967a2a2c20aa3f3932dbdda89b744f47e9
akasen@lenovo-x250 <akasen.org@gmail.com>
2022-01-22 10:25:05 +0900

    removed barcode scanner from stock page because the detection rate is so low (it needs further research)

--- components/pages/stock.tsx
+++ components/pages/stock.tsx
@@ -11,9 +11,6 @@ export const StockPage = () => {
   const {data: productsData} = useSWR("https://atre.blob.core.windows.net/products-data/productsData.json", fetcher)
   const {data: stockData} = useSWR("/api/azure/getStock", fetcher)
 
-  const scannerRef = useRef(null)
-  const [scanning, setScanning] = useState(false)
-
   const [inputText, setInputText] = useState("")
   const [isJanCode, setIsJanCode] = useState(false)
   const [searchBy] = useDebounce(inputText, 1000)
@@ -22,61 +19,43 @@ export const StockPage = () => {
     setInputText(event.target.value)
     setIsJanCode(/[0-9]+/.test(event.target.value))
   }
-  const onDetected = (result: string) => {
-    setInputText(result)
-    setIsJanCode(true)
+  const onClickClear = () => {
+    setInputText("")
   }
+
   return <Box>
-    <Box sx={{display: "flex", justifyContent: "center"}}>
-      <div style={{width: 300, height: 300}}>
-        <div ref={scannerRef} style={{position: "relative"}}>
-          <canvas className={"drawingBuffer"} style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            width: "100%"
-          }} width={300} height={300} />
-          {scanning && <Scanner
-            scannerRef={scannerRef}
-            onDetected={onDetected}
-          />}
-        </div>
-      </div>
-    </Box>
+    <Typography variant={"h6"} sx={{ borderBottom: "1px solid darkgray" }}>在庫数を検索する</Typography>
     <Box sx={{
-      display: "flex",
-      justifyContent: "center"
-    }}>
-      <Button onClick={()=>setScanning(true)} disabled={scanning}>バーコードをスキャンする</Button>
+        m: 2,
+        '& .MuiTextField-root': { mt: 1},
+      }}>
+        <TextField
+          fullWidth={true}
+          value={inputText}
+          onChange={handleSearchByChange}
+          placeholder={"商品名、JANコードを入力"}
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+      </Box>
+      <Box sx={{
+        display: "flex",
+        justifyContent: "space-around",
+        m: 1,
+      }}>
+        <Button color="secondary" variant="outlined" onClick={onClickClear}>クリア</Button>
+      </Box>
+      {!productsData && <Box sx={{display: "flex", justifyContent: "center"}}><Typography variant={"body1"}>商品データを読み込み中...</Typography></Box>}
+      {!stockData && <Box sx={{display: "flex", justifyContent: "center"}}><Typography variant={"body1"}>在庫データを読み込み中...</Typography></Box>}
+      {productsData && stockData && !inputText && <Box sx={{display: "flex", justifyContent: "center", mt: 1}}>
+        <Typography variant="body1">検索ワード (商品名、JANコード) を入力してください。</Typography>
+        </Box>}
+      {productsData && stockData && inputText && <DisplaySearchResultByItemName
+        searchBy={searchBy}
+        isJanCode={isJanCode}
+        products={productsData.data}
+        stockData={stockData.result}
+      />}
     </Box>
-    <hr />
-    <Box sx={{
-      m: 2,
-      '& .MuiTextField-root': { mt: 1},
-    }}>
-      <TextField
-        fullWidth={true}
-        value={inputText}
-        onChange={handleSearchByChange}
-        InputLabelProps={{
-          shrink: true
-        }}
-      />
-    </Box>
-    <Box sx={{
-      display: "flex",
-      justifyContent: "space-around",
-      m: 1,
-    }}>
-      <Button color="primary" variant="outlined">在庫数を更新</Button>
-    </Box>
-    {!productsData && <Box sx={{display: "flex", justifyContent: "center"}}><Typography variant={"body1"}>商品データを読み込み中...</Typography></Box>}
-    {!stockData && <Box sx={{display: "flex", justifyContent: "center"}}><Typography variant={"body1"}>在庫データを読み込み中...</Typography></Box>}
-    {productsData && stockData && inputText && <DisplaySearchResultByItemName
-      searchBy={searchBy}
-      isJanCode={isJanCode}
-      products={productsData.data}
-      stockData={stockData.result}
-    />}
-  </Box>
 }
