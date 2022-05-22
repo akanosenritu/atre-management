d11ceecbddfa13e364b41256f205604d32433e89
akasen@lenovo-x250 <akasen.org@gmail.com>
2022-05-22 16:56:11 +0900

    updated for gotoyome2022

--- pages/csv.tsx
+++ pages/csv.tsx
@@ -14,6 +14,9 @@ type Datum = {
 type LatestSalesDataInitial = {
   status: "initial"
 }
+type LatestSalesDataDownloading = {
+  status: "downloading"
+}
 type LatestSalesDataRetrieved = {
   status: "retrieved",
   createdAt: string,
@@ -25,9 +28,9 @@ type LatestSalesDataNoData = {
 type LatestSalesDataRetrievalFailed = {
   status: "failed"
 }
-type LatestSalesData = LatestSalesDataInitial | LatestSalesDataRetrieved | LatestSalesDataNoData | LatestSalesDataRetrievalFailed
+type LatestSalesData = LatestSalesDataDownloading | LatestSalesDataInitial | LatestSalesDataRetrieved | LatestSalesDataNoData | LatestSalesDataRetrievalFailed
 
-const retrieveLatestSalesData = async (): Promise<LatestSalesData> => {
+const retrieveTodaysSalesData = async (): Promise<LatestSalesData> => {
   // get information of the latest data
   const todayString = getTodayString()
   const res = await fetch(`https://atre.blob.core.windows.net/sales-data/${todayString}.json`)
@@ -43,6 +46,26 @@ const retrieveLatestSalesData = async (): Promise<LatestSalesData> => {
   }
 }
 
+const retrieveLatestSalesData = async (): Promise<LatestSalesData> => {
+  const res1 = await fetch("/api/azure/downloadSalesData")
+  const res2 = await fetch("/api/azure/processSalesDataCSV", {
+    method: "POST",
+    body: (await res1.json()).data,
+    headers: {
+      "Content-Type": "application/text",
+    }
+  })
+  if (!res2.ok) {
+    return {status: "failed"}
+  }
+  return {
+    status: "retrieved",
+    createdAt: new Date().toISOString(),
+    data: (await res2.json()).data
+  }
+}
+
+
 const Page: NextPage = () => {
   const [autoDownloadedLatestSalesData, setAutoDownloadedLatestSalesData] = useState<LatestSalesData>({status: "initial"})
   const onClickCopyButton = () => {
@@ -54,20 +77,21 @@ const Page: NextPage = () => {
 
   // load the latest file from azure storage
   // only run it initially
-  useEffect(() => {
+  const onClickDownload = () => {
+    setAutoDownloadedLatestSalesData({status: "downloading"})
     retrieveLatestSalesData()
       .then(result => setAutoDownloadedLatestSalesData(result))
-  }, [])
+  }
 
   return <Layout>
-    <Typography variant={"h6"} sx={{borderBottom: "1px solid darkgray"}}>自動ダウンロードされたデータを使用する</Typography>
-    <Typography variant={"body2"} sx={{paddingLeft: 3, paddingTop: 1}}>
-      スマレジ上から自動的にダウンロードされたデータを使用します。データは21時4分に自動的にダウンロードされ、クラウド上に保存されています。
-      クラウド上にデータが作成される21時4分から23時59分まで利用できますが、それ以外の時間には使用できません。
-    </Typography>
+    <Typography variant={"h6"} sx={{borderBottom: "1px solid darkgray"}}>ダウンロードされたデータを使用する</Typography>
+    <div style={{display: "flex", justifyContent: "center"}}>
+      <button onClick={onClickDownload}>データをダウンロードする</button>
+    </div>
     <div style={{display: "flex", margin: 5, justifyContent: "center"}}>
       <div>
-        {autoDownloadedLatestSalesData.status === "initial" && "データをダウンロード中です"}
+        {autoDownloadedLatestSalesData.status === "initial" && "データがダウンロードされていません"}
+        {autoDownloadedLatestSalesData.status === "downloading" && "データをダウンロード中です"}
         {autoDownloadedLatestSalesData.status === "noData" && "データが見つかりませんでした"}
         {autoDownloadedLatestSalesData.status === "retrieved" && `データの作成時刻: ${autoDownloadedLatestSalesData.createdAt}`}
         {autoDownloadedLatestSalesData.status === "failed" && "データのダウンロードに失敗しました"}
